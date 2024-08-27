42b9a820c16bab595eabe59be2889ee040482abe
crashmax-dev <crashmax.dev@gmail.com>
2024-08-27 12:18:12 +0000

    Deploying to gh-pages from @ crashmax-dev/steam-gifts-extended@a89f5545d55a7674d6fe8b2468d7e018d32ffd2b 🚀

---
+++ steam-gifts-extended.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        steam-gifts-extended
+// @version     2.0.0
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/steam-gifts-extended/
+// @icon        https://cdn.steamgifts.com/img/favicon.ico
+// @match       https://www.steamgifts.com/*
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/steam-gifts-extended/steam-gifts-extended.meta.js
+// @downloadURL https://crashmax-dev.github.io/steam-gifts-extended/steam-gifts-extended.user.js
+// ==/UserScript==
--- steamgifts-extended.meta.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name        steamgifts-extended
-// @version     1.0.0
-// @author      Vitalij Ryndin
-// @description 🎮 Userscript for SteamGifts.com
-// @homepage    https://crashmax-off.github.io/SteamGiftsExtended
-// @match       *://www.steamgifts.com/*
-// @icon        https://crashmax-off.github.io/SteamGiftsExtended/icons/steamgifts.svg
-// ==/UserScript==
